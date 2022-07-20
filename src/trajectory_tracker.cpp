dd224bbbfd49e9c240a80c4b45ff86cda47a643a
fupbot <ugaldepereira@gmail.com>
2022-07-20 19:41:46 -0300

    codigo para criar trajetorias e occupancy grid

---
+++ src/trajectory_tracker.cpp
@@ -0,0 +1,10 @@
+/*Code for simple navigation of Pioneer 3DX robot in MobileSim simulation.
+Robot smoves forward, senses two frontal sonars and turns right 90 deg if there's an obstacle.
+Master's degree course of Mobile Robotis @ UFRGS, Brazil.
+Author: fupbot (github)
+Date: July, 2022
+*/
+
+#include<ros/ros.h>
+//#include<geometry_msgs/Twist.h>
+//#include<sensor_msgs/PointCloud.h>
